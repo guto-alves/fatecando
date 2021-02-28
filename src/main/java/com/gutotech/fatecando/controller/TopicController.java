fbb10efa135fceb6ee098e7448a98d4f93be69b1
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2021-02-27 22:37:41 -0300

    Implement Quiz with the topic questions

--- src/main/java/com/gutotech/fatecando/controller/TopicController.java
+++ src/main/java/com/gutotech/fatecando/controller/TopicController.java
@@ -3,11 +3,16 @@ package com.gutotech.fatecando.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gutotech.fatecando.model.Topic;
+import com.gutotech.fatecando.model.UploadStatus;
+import com.gutotech.fatecando.service.DisciplineService;
+import com.gutotech.fatecando.service.QuestionService;
 import com.gutotech.fatecando.service.TopicService;
 
 @Controller
@@ -17,13 +22,35 @@ public class TopicController {
 	@Autowired
 	private TopicService topicService;
 
+	@Autowired
+	private DisciplineService disciplineService;
+
+	@Autowired
+	private QuestionService questionService;
+
 	@GetMapping("{id}")
-	public String showDisciplines(@PathVariable("id") Long id, Model model) {
+	public String showTopic(@PathVariable("id") Long id, Model model) {
 		Topic topic = topicService.findById(id);
 
 		model.addAttribute("topic", topic);
+		model.addAttribute("questions", questionService.findAllByTopic(topic));
 
 		return "disciplines/topic-details";
+	}
+
+	@GetMapping
+	public String initCreationForm(Model model) {
+		model.addAttribute("topic", new Topic());
+		model.addAttribute("disciplines", disciplineService.findAll());
+		return "admin/topic-edit";
+	}
+
+	@PostMapping
+	public String processCreationForm(Topic topic, Model model, BindingResult bindingResult) {
+		topic.setStatus(UploadStatus.WAITING_FOR_RESPONSE);
+		topic.setRequired(false);
+		topicService.save(topic);
+		return "redirect:/dashboard";
 	}
 
 }
---
+++ src/main/resources/static/js/topic-details.js
@@ -0,0 +1,95 @@
+let currentQuestionIndex = -1;
+let questionsLenght = Object.keys(questions).length;
+let feedbacks = [];
+
+$('#startQuiz').click(function() {
+	$(this).remove();
+
+	nextQuestion();
+
+	$('#questionContainer').show();
+});
+
+function nextQuestion() {
+	currentQuestionIndex = ++currentQuestionIndex % questionsLenght;
+
+	$('#questionIndex').text('Questão ' + (currentQuestionIndex + 1) + ' de ' + questionsLenght);
+
+	let currentQuestion = questions[currentQuestionIndex];
+
+	$('#question').html(currentQuestion.description);
+
+	// Removes old alternatives
+	$('.form-check').remove();
+
+	// Add new alternatives
+	currentQuestion.alternatives.forEach(alternative => {
+		let alternativeRadio = `
+			<div class="form-check mb-3">
+				<input class="form-check-input" type="radio"
+					name="alternativeRadio" value="` + alternative.id + `">
+				<label class="form-check-label">` + alternative.description + `</label>
+			</div>		
+		`;
+
+		$('#questionContainer').append(alternativeRadio);
+	});
+
+	$('#nextQuestion').hide();
+	$('#answerQuestion').show();
+}
+
+$('#answerQuestion').click(() => {
+	let alternativeId = $('input[name="alternativeRadio"]:checked').val();
+	let questionId = questions[currentQuestionIndex].id;
+
+	$.post(
+		'http://localhost:8080/questions/' + questionId + '/answer/' + alternativeId
+	).done(feedback => {
+		feedbacks.push(feedback);
+
+		let color = feedback.correct ? 'success' : 'danger';
+		let text = feedback.correct ? 'Correto' : 'Errado';
+
+		let htmlFeedback = `
+			<div class="border border-2 border- ` + color + ` p-2 mt-2 rounded ">
+				<span>
+					<b class="text-` + color + `">` + text + `</b>
+					<br>
+					` + feedback.feedback + `
+				</span>
+			</div>
+		`;
+
+		$('input[name="alternativeRadio"]:checked').closest('.form-check').append(htmlFeedback);
+
+		if (currentQuestionIndex + 1 == questionsLenght) { // Quiz is over
+			$('#answerQuestion').remove();
+			$('#nextQuestion').remove();
+			$('#finishQuiz').show();
+		} else {
+			$('#answerQuestion').hide();
+			$('#nextQuestion').show();
+		}
+	}).fail(() => {
+		console.log('Error ao registrar resposta. Por favor, tente mais tarde.');
+	});
+});
+
+$('#finishQuiz').click(() => {
+	$('#finishQuiz').remove();
+	$('#questionContainer').remove();
+
+	let rightAnswers = feedbacks.reduce((total, feedback) => total + (feedback.correct ? 1 : 0), 0);
+	
+	let result = rightAnswers + '/' + questionsLenght;
+
+	$('#quizResult').append(
+		`<span class="h1 fw-bolder">` + result + `</span>
+		<br>
+		<div class="text-muted text-right mt-3">Finalizado em: ` + new Date().toLocaleString() + `</div>`
+	);
+
+	$('#quizResult').show();
+});
+
