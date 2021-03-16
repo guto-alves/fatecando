e270c0e55b5f5735102404cafbe17d69ea83253b
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2021-03-15 22:01:18 -0300

    Change the dashboard page

--- src/main/java/com/gutotech/fatecando/controller/DashboardController.java
+++ src/main/java/com/gutotech/fatecando/controller/DashboardController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gutotech.fatecando.model.Course;
 import com.gutotech.fatecando.service.CourseService;
 import com.gutotech.fatecando.service.DisciplineService;
 import com.gutotech.fatecando.service.InstitutionService;
@@ -25,10 +26,17 @@ public class DashboardController {
 
 	@GetMapping
 	public String home(Model model) {
-		model.addAttribute("disciplines", disciplineService.findAllAccessed());
-		model.addAttribute("courses", courseService.findAll());
-		model.addAttribute("institutions", institutionService.findAll());
-		return "dashboard";
+//		model.addAttribute("disciplines", disciplineService.findAllAccessed());
+//		model.addAttribute("courses", courseService.findAll());
+//		model.addAttribute("institutions", institutionService.findAll());
+//		return "dashboard";
+		
+		Course course = courseService.findById(1L);
+
+		model.addAttribute("course", course);
+		model.addAttribute("disciplines", disciplineService.findAllByCourse(course));
+
+		return "disciplines/course-details";
 	}
 
 }
