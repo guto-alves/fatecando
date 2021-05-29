a948a3eaf3b0a3db3c4bff1107770b24901b2353
Gustavo Alves <gustavo.almeida13@fatec.sp.gov.br>
2021-05-29 00:14:53 -0300

    Create page to manage users

---
+++ src/main/java/com/gutotech/fatecando/controller/admin/UserAdminController.java
@@ -0,0 +1,30 @@
+package com.gutotech.fatecando.controller.admin;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gutotech.fatecando.model.User;
+import com.gutotech.fatecando.service.UserService;
+
+@Controller
+@RequestMapping("admin/users")
+public class UserAdminController {
+
+	@Autowired
+	private UserService userService;
+
+	@ModelAttribute("users")
+	public List<User> getUsers() {
+		return userService.findAll();
+	}
+
+	@GetMapping
+	public String showCoursesPage() {
+		return "admin/users";
+	}
+}
