b38c5358e8099c2072d4069015c227a6e3c34321
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2021-11-03 20:42:59 -0300

    Change MessageController RequestMapping name

--- src/main/java/com/gutotech/fatecando/controller/MessageController.java
+++ src/main/java/com/gutotech/fatecando/controller/MessageController.java
@@ -14,7 +14,7 @@ import com.gutotech.fatecando.model.Message;
 import com.gutotech.fatecando.service.ChatService;
 
 @Controller
-@RequestMapping("user/messages")
+@RequestMapping("messages")
 public class MessageController {
 
 	@GetMapping
